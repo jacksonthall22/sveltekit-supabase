b6b379a25f64e5e8b3e7a01ed62166e48ad7a624
jackson <jacksonthall22@gmail.com>
2025-05-06 20:18:12 -0400

    Clean up error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import { page } from '$app/stores';
+  import { page } from '$app/state'
 </script>
 
-<div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-        <div class="max-w-md flex flex-col gap-2">
-            <h1 class="text-5xl font-bold mb-3">Oops!</h1>
-            <code>{$page.status}: {$page.error?.message}</code>
-            <p>Something went wrong. Please try again later.</p>
-        </div>
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="flex max-w-md flex-col gap-2">
+      <h1 class="mb-3 text-5xl font-bold">Oops!</h1>
+      <code>{page.status}: {page.error?.message}</code>
+      <p>Something went wrong. Please try again later.</p>
     </div>
+  </div>
 </div>
