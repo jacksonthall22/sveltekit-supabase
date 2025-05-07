a5ed9834f556b907ee5ec26c69e4070f8aba9294
jackson <jacksonthall22@gmail.com>
2025-05-06 20:11:57 -0400

    Return `fail` instead of `error` for form action

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,9 +1,5 @@
-import { redirect } from '@sveltejs/kit'
-
+import { redirect, fail } from '@sveltejs/kit'
 import type { Actions } from './$types'
-import { PRIVATE_HCAPTCHA_SECRET_KEY } from '$env/static/private'
-
-export const ssr = false;
 
 export const actions: Actions = {
   signup: async ({ request, locals: { supabase } }) => {
@@ -16,27 +12,10 @@ export const actions: Actions = {
     // Verify hCaptcha before proceeding
     if (!hcaptchaToken) {
       // console.error('test: Missing hCaptcha token')
-      return redirect(303, '/auth/error')
+      return fail(400, { hcaptchaResponse: 'Missing hCaptcha token' })
     } else {
       // console.log(`[AUTH +page.server.ts] test: signup() hcaptchaToken: ${hcaptchaToken}`)
     }
-
-    // const verifyRes = await fetch('https://hcaptcha.com/siteverify', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    //   body: new URLSearchParams({
-    //     secret: PRIVATE_HCAPTCHA_SECRET_KEY,
-    //     response: hcaptchaToken
-    //   })
-    // })
-    // const verifyData = await verifyRes.json()
-    // console.log(
-    //   `[AUTH +page.server.ts] test: hcaptcha response JSON: ${JSON.stringify(verifyData)}`
-    // )
-    // if (!verifyData.success) {
-    //   console.error('hCaptcha verification failed', verifyData)
-    //   return redirect(303, '/auth/error')
-    // }
 
     const { error } = await supabase.auth.signUp({
       email,
@@ -47,11 +26,11 @@ export const actions: Actions = {
 
     if (error) {
       // console.error(`[AUTH +page.server.ts] error: ${error}`)
-      return redirect(303, '/auth/error')
-    } else {
-      // console.log(`[AUTH +page.server.ts] test: signup() SUCCESS! redirecting to '/'...`)
-      return redirect(303, '/')
+      return fail(400, { signUpError: error })
     }
+    
+    // console.log(`[AUTH +page.server.ts] test: signup() SUCCESS! redirecting to '/'...`)
+    return { success: true }
   },
   login: async ({ request, locals: { supabase } }) => {
     const formData = await request.formData()
@@ -77,7 +56,6 @@ export const actions: Actions = {
     })
 
     // console.log(`[AUTH +page.server.ts] test: login() error: ${error}`)
-
     if (error) {
       console.error(error)
       return redirect(303, '/auth/error')
