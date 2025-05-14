a26ceea36ddd59b53db13ea8b4ef3ddb26c0a8a0
jackson <jacksonthall22@gmail.com>
2025-05-13 20:34:13 -0400

    Improve `HCaptcha` handling (WIP)

--- src/lib/components/HCaptcha.svelte
+++ src/lib/components/HCaptcha.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-  import { PUBLIC_HCAPTCHA_SITE_KEY } from '$env/static/public'
+  import { PUBLIC_HCAPTCHA_SITE_KEY, PUBLIC_USE_HCAPTCHA } from '$env/static/public'
   import { H_CAPTCHA_ELEMENT_ID, H_CAPTCHA_TOKEN_INPUT_NAME } from '$lib/constants'
   import { onMount } from 'svelte'
 
-  let token = $state(undefined)
+  interface Props {
+    token: string
+  }
+  let { token = $bindable() }: Props = $props()
 
   onMount(() => {
+    if (!PUBLIC_USE_HCAPTCHA) return
+
     window.hcaptcha.render(H_CAPTCHA_ELEMENT_ID, {
       sitekey: PUBLIC_HCAPTCHA_SITE_KEY,
       callback: (response) => {
@@ -17,10 +22,12 @@
 </script>
 
 <svelte:head>
-  <script src="https://js.hcaptcha.com/1/api.js?render=explicit" async defer></script>
+  <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 </svelte:head>
 
-<div class="mt-4 w-full">
-  <div id={H_CAPTCHA_ELEMENT_ID} class="h-captcha"></div>
-  <input type="hidden" name={H_CAPTCHA_TOKEN_INPUT_NAME} value={token} />
-</div>
+{#if PUBLIC_USE_HCAPTCHA}
+  <div class="mt-4 w-full">
+    <div class="h-captcha" data-sitekey={PUBLIC_HCAPTCHA_SITE_KEY}></div>
+    <input type="hidden" />
+  </div>
+{/if}
