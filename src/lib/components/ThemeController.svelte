c17c0e18cf5e20c8b6693413c7aed94253555a43
jackson <jacksonthall22@gmail.com>
2025-05-16 22:37:28 -0400

    Make theme switch smaller

--- src/lib/components/ThemeController.svelte
+++ src/lib/components/ThemeController.svelte
@@ -16,7 +16,7 @@
 
   <!-- sun icon -->
   <svg
-    class="swap-off h-10 w-10 fill-current"
+    class="swap-off h-8 w-8 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
   >
@@ -27,7 +27,7 @@
 
   <!-- moon icon -->
   <svg
-    class="swap-on h-10 w-10 fill-current"
+    class="swap-on h-8 w-8 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
   >
