c04cc4c344f13f1e074df2ee3c08c29753270404
jackson <jacksonthall22@gmail.com>
2025-05-17 09:36:10 -0400

    Fix/improve root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,25 @@
   let { data } = $props()
   let user = $derived(data.user)
 
-  const { form, errors, constraints, message, submitting, delayed, enhance } = superForm(data.form);
-
+  const { form, errors, constraints, message, submitting, delayed, enhance } = $derived(
+    superForm(data.form, {
+      // These allow form to be submitted multiple times and prevent page's `load()` function from re-running.
+      // Weirdly, the form will disappear after a successful submission if these are not set.
+      invalidateAll: false,
+      resetForm: false,
+    }),
+  )
   const avatar = ''
 </script>
 
-{#if user}
-  <div class="bg-base-100 border-base-300 collapse border">
-    <input type="checkbox" checked />
-    <div class="collapse-title font-semibold">Debug info</div>
-    <div class="collapse-content text-sm">
-      <SuperDebug bind:data={$form} />
-    </div>
-  </div>
+{#if $message}
+  <h3>{$message}</h3>
+{/if}
 
+{#if user}
   <div class="card card-lg">
-    <div class="card-content">
-      <form method="post" action='?' use:enhance>
+    <div class="card-content mx-auto flex flex-col gap-4">
+      <form method="post" use:enhance>
         <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend class="fieldset-legend">Profile</legend>
 
@@ -35,7 +37,7 @@
           {:else}
             <div class="avatar avatar-online avatar-placeholder max-w-fit">
               <div class="bg-neutral text-neutral-content w-16 rounded-full">
-                <span class="text-xl">C</span>
+                <span class="text-xl">{$form.firstName?.at(0) ?? '?'}</span>
               </div>
             </div>
           {/if}
@@ -45,21 +47,28 @@
             name="firstName"
             type="text"
             bind:value={$form.firstName}
+            {...$constraints.firstName}
+            aria-invalid={$errors.firstName ? 'true' : undefined}
             class="input w-full"
             placeholder="Roofus"
           />
+          {#if $errors.firstName}
+            <p class="text-error text-sm">{JSON.stringify($errors.firstName)}</p>
+          {/if}
 
           <label for="lastName" class="label">Last name</label>
           <input
             name="lastName"
             type="text"
             bind:value={$form.lastName}
+            {...$constraints.lastName}
+            aria-invalid={$errors.lastName ? 'true' : undefined}
             class="input"
             placeholder="n'Doofus"
           />
-
-          <label for="email" class="label">Email</label>
-          <input name="email" type="email" bind:value={$form.email} class="input" placeholder="Email" />
+          {#if $errors.lastName}
+            <p class="text-error text-sm">{JSON.stringify($errors.lastName)}</p>
+          {/if}
 
           <button type="submit" disabled={$submitting} class="btn btn-accent">
             {#if $delayed}
@@ -69,7 +78,14 @@
             {/if}
           </button>
         </fieldset>
+        {#if $errors._errors}
+          <p class="text-error text-sm">{JSON.stringify($errors._errors)}</p>
+        {/if}
       </form>
+
+      <div class="w-xs">
+        <SuperDebug bind:data={$form} collapsible />
+      </div>
     </div>
   </div>
 {:else}
@@ -80,7 +96,7 @@
         <div>+</div>
         <Icon icon="logos:supabase-icon" class="h-24 w-24" />
       </div>
-      <a href="/auth" class="btn btn-lg btn-block">Login</a>
+      <a href="/auth/signIn" class="btn btn-lg btn-block">Login</a>
     </div>
   </div>
 {/if}
