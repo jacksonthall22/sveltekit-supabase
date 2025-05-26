abdafdbc2125d319035b844493b5df5fbaabe158
jackson <jacksonthall22@gmail.com>
2025-05-26 09:13:19 -0400

    Improve `/private` page

--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <div class="card">
-  <div class=" card-title">Private page for user 😏</div>
   <div class="card-body">
+    <h1 class="card-title">Page data</h1>
+    <p>
+      If you're seeing this page, it means the <code>+hooks.server.ts</code>
+      <code>authGuard()</code> function passed. This route is inaccessible when logged out.
+    </p>
     <code class="whitespace-pre">{JSON.stringify(data, undefined, 2)}</code>
   </div>
 </div>
