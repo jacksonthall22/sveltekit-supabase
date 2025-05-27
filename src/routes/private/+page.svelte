c61d32abf9932473a24827e42ec3fc9a817a584d
jackson <jacksonthall22@gmail.com>
2025-05-27 06:34:54 -0400

    Style tweaks

--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -4,11 +4,11 @@
 
 <div class="card">
   <div class="card-body">
-    <h1 class="card-title">Page data</h1>
     <p>
       If you're seeing this page, it means the <code>+hooks.server.ts</code>
       <code>authGuard()</code> function passed. This route is inaccessible when logged out.
     </p>
+    <code class="card-title">page data:</code>
     <code class="whitespace-pre">{JSON.stringify(data, undefined, 2)}</code>
   </div>
 </div>
