3a5288968b9d0d2f7e2be1132dd59741211d33c0
jackson <jacksonthall22@gmail.com>
2025-06-01 20:40:15 -0400

    Add `enhancedImages()`, `kitRoutes()` plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { enhancedImages } from '@sveltejs/enhanced-img'
+import { kitRoutes } from 'vite-plugin-kit-routes'
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), enhancedImages(), sveltekit(), kitRoutes()],
 })
