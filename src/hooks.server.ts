510b890c1f5c6ee83ba84efe5d9fbed2c9ada876
jackson <jacksonthall22@gmail.com>
2025-06-01 20:40:36 -0400

    Golf

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -65,18 +65,15 @@ const supabase: Handle = async ({ event, resolve }) => {
       data: { session },
     } = await event.locals.supabase.auth.getSession()
 
-    if (!session) {
-      return { session: null, user: null }
-    }
+    if (!session) return { session: null, user: null }
 
     const {
       data: { user },
       error,
     } = await event.locals.supabase.auth.getUser()
-    if (error) {
-      // JWT validation has failed
-      return { session: null, user: null }
-    }
+
+    // Check if JWT validation has failed
+    if (error) return { session: null, user: null }
 
     return { session, user }
   }
