62e865278c7c163cbc608289c6668783c45df06f
jackson <jacksonthall22@gmail.com>
2025-06-01 20:39:35 -0400

    Better formatting in `/private`

--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -2,13 +2,18 @@
   let { data } = $props()
 </script>
 
-<div class="card">
-  <div class="card-body">
-    <p>
-      If you're seeing this page, it means the <code>+hooks.server.ts</code>
-      <code>authGuard()</code> function passed. This route is inaccessible when logged out.
-    </p>
-    <code class="card-title">page data:</code>
-    <code class="whitespace-pre">{JSON.stringify(data, undefined, 2)}</code>
-  </div>
+<div class="w-full">
+  <p>
+    If you're seeing this page, it means the <code>+hooks.server.ts</code>
+    <code>authGuard()</code> function passed. This route is inaccessible when logged out.
+  </p>
+  
+  <h1 class='text-2xl font-bold mt-6 my-2'>
+    <code>Page Data</code>
+  </h1>
+  <p class='overflow-x-scroll scroll-x-auto bg-base-200 p-2'>
+    <code class="w-full whitespace-pre overflow-y-scroll overflow-hidden">
+      {JSON.stringify(data, undefined, 2)}
+    </code>
+  </p>
 </div>
