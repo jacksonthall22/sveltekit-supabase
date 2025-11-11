90558143c4b3f34eaa232e184524230296605df9
Jackson Hall <jacksonthall22@gmail.com>
2025-11-11 18:17:35 -0500

    Remove note about patched `drizzle-kit` issue

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,19 +1,3 @@
-// Note: Drizzle is currently bugged with how it sets RLS policies using `drizzle-kit push`:
-//
-//    https://github.com/drizzle-team/drizzle-orm/issues/3504
-//
-// Until that's resolved, you can either use migrations, or if you don't want to deal with them,
-// this flow worked for me for TESTING ONLY (WILL DELETE YOUR DATABASES AND ALL DATA IN SUPABASE):
-//
-//   1. Make changes here, to schema, RLS, etc.
-//   2. Comment out this whole file
-//   3. `drizzle-kit push` - DESTRUCTIVE, deletes all your tables and their data in Supabase
-//   4. Uncomment this file
-//   5. `rm -rf drizzle` - DESTRUCTIVE, removes any previous migrations stored in `drizzle` folder
-//   5. `drizzle-kit generate && drizzle-kit migrate` - generates SQL migrations (where RLS gets set correctly) and "pushes" changes
-//
-// Note: Sometimes you may also want to delete the `__drizzle_migrations` table under the `drizzle` schema in Supabase Table Editor
-
 import { eq, relations } from 'drizzle-orm'
 import { pgPolicy, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 import { authenticatedRole, authUid, authUsers } from 'drizzle-orm/supabase'
