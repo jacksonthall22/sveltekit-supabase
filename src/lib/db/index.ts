79599eb072719147d10e1778cd721b0cc4455abb
Jackson Hall <jacksonthall22@gmail.com>
2026-01-11 02:02:47 -0600

    Rename database env var

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
-import { PRIVATE_SUPABASE_DATABASE_URL } from '$env/static/private'
+import { PRIVATE_DATABASE_URL } from '$env/static/private'
 
-export const client = postgres(PRIVATE_SUPABASE_DATABASE_URL, { prepare: false })
+export const client = postgres(PRIVATE_DATABASE_URL, { prepare: false })
 export const db = drizzle(client, { schema })
